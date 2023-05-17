e0e319b1be190dcf96b5cf77f4e486b861a52de8
Christopher Lee <clee2k6@outlook.com>
2023-05-16 22:08:25 -0400

    changed ui a bit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
